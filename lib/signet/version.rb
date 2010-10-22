53caac6b336ef98e625e8186f216365499a9ba5d
Bob Aman <bobaman@google.com>
2010-10-22 00:33:42 +0000

    Updated CHANGELOG
    git-svn-id: https://oauth-signet.googlecode.com/svn/trunk@7 7af18d84-67ec-4026-b916-6c96ec723f59

--- lib/signet/version.rb
+++ lib/signet/version.rb
@@ -18,7 +18,7 @@ unless defined? Signet::VERSION
     module VERSION #:nodoc:
       MAJOR = 0
       MINOR = 1
-      TINY  = 2
+      TINY  = 3
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
