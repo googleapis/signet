9f3f4a662d04283cb7e383aadde36140a342c407
Steven Bazyl <sqrrrl@gmail.com>
2014-12-05 14:01:23 -0800

    Release 0.6.0

--- lib/signet/version.rb
+++ lib/signet/version.rb
@@ -17,8 +17,8 @@ unless defined? Signet::VERSION
   module Signet
     module VERSION
       MAJOR = 0
-      MINOR = 5
-      TINY  = 1
+      MINOR = 6
+      TINY  = 0
 
       STRING = [MAJOR, MINOR, TINY].join('.')
     end
