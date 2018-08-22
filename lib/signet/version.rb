e355f1de3eed6656e8ddfb5b9d05e67d643c73d2
TheRoyalTnetennba <paye@google.com>
2018-08-22 11:50:06 -0700

    increment version number

--- lib/signet/version.rb
+++ lib/signet/version.rb
@@ -18,7 +18,7 @@ unless defined? Signet::VERSION
     module VERSION
       MAJOR = 0
       MINOR = 9
-      TINY  = 0
+      TINY  = 1
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
