adc2698465c0d98334c838052b52b6788f7c75ed
Daniel Azuma <dazuma@gmail.com>
2018-09-13 12:37:13 -0700

    Release version 0.9.2 (#118)

--- lib/signet/version.rb
+++ lib/signet/version.rb
@@ -18,7 +18,7 @@ unless defined? Signet::VERSION
     module VERSION
       MAJOR = 0
       MINOR = 9
-      TINY  = 1
+      TINY  = 2
       PRE   = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
