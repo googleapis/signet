aa86c645e49e02ac77ed535a68cc6bd658dfa113
Daniel Azuma <dazuma@google.com>
2021-08-31 18:20:03 -0700

    chore: Update release scripts (#195)

--- .toys/release.rb
+++ .toys/release.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,22 +14,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-expand :clean, paths: :gitignore
-
-expand :rspec do |t|
-  t.libs = ["lib", "spec"]
-  t.use_bundler
-end
-
-expand :rubocop, bundler: true
-
-expand :yardoc do |t|
-  t.generate_output_flag = true
-  t.fail_on_warning = true
-  t.use_bundler
-end
-alias_tool :yard, :yardoc
-
-expand :gem_build
-
-expand :gem_build, name: "install", install_gem: true
+load_git remote: "https://github.com/googleapis/google-cloud-ruby.git",
+         path: ".toys/release"
