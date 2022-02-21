120c0639cd3398f74a3481b106d5ba88bbff06d8
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2022-02-21 10:47:53 +0000

    Remove handling of `Array` being passed as `options[:request]` to `Server`.
    The possibility to pass a `Faraday::Request` was introduced [here](https://github.com/iMacTia/signet/commit/bb449aa64ca4fcbe2d7485370f871f444c388888#diff-cc62e9a1887143d3c39c443e1906d3edf397f78e6c098d9ce21a49a2b0bdaeddR150-R158), but the `Array` option was also left.
    Unfortunately, a few lines below the decomposition of `request` (which works in case of an array was replaced with method calls to `method`, `path` and so on... that works with a `Faraday::Request` but raises an `ArgumentError` with an `Array`!
    The `Array` is simply not supported anymore, but there was a test that still passes one whose intention seems to be to test the check on https://github.com/iMacTia/signet/blob/main/lib/signet/oauth_1/server.rb#L182. That line is never reached though, as `ArgumentError` is instead raised while calling `request.method` against the `Array`.

--- lib/signet/oauth_1/server.rb
+++ lib/signet/oauth_1/server.rb
@@ -152,7 +152,7 @@ module Signet
       # @return [Hash] normalized request components
       def verify_request_components options = {}
         if options[:request]
-          if options[:request].is_a?(Faraday::Request) || options[:request].is_a?(Array)
+          if options[:request].is_a? Faraday::Request
             request = options[:request]
           elsif options[:adapter]
             request = options[:adapter].adapt_request options[:request]
